b4e95670c9220bd2253dbf0c1f15a6fb46bb0d66
domonk-glitch <domonk@gmail.com>
2026-06-05 14:17:03 -0400

    H: Got block type values added and checked for.

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,42 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+def block_to_block_type(block: str):
+    
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+    if block.startswith('```\n') and block.endswith('```'):
+        return BlockType.CODE
+
+    lines = block.split('\n')
+    if block.startswith('>'):
+        for line in lines:
+            if not line.startswith('>'):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    
+    if block.startswith('- '):
+        for line in lines:
+            if not line.startswith('- '):
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+    
+    if block.startswith('1. '):
+        n = 1
+        for line in lines:
+            if not line.startswith(f"{n}. "):
+                return BlockType.PARAGRAPH
+            n += 1
+        return BlockType.ORDERED_LIST
+    
+    return BlockType.PARAGRAPH
 
 
 def markdown_to_blocks(markdown: str):
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import markdown_to_blocks, block_to_block_type
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks1(self):
@@ -65,4 +65,63 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+
+
+    def test_block_to_block_type1(self):
+        md = "###### Heading test"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(
+            blocktype,
+            blocktype.HEADING
+        )
+
+    def test_block_to_block_type2(self):
+        md = "```\nThis should be a code block.\nMultiple lines for the test of course.\n```"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(
+            blocktype,
+            blocktype.CODE
+        )
+
+    def test_block_to_block_type3(self):
+        md = """> Quote testing.
+> Gotta make sure all the lines are contained within.
+oops! missed one!
+> another quote.
+"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(
+            blocktype,
+            blocktype.PARAGRAPH
+        )
+
+    def test_block_to_block_type4(self):
+        md = "- Unordered lists are fun.\n- No. No they are not.\n- Maybe"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(
+            blocktype,
+            blocktype.UNORDERED_LIST
+        )
+
+    def test_block_to_block_type5(self):
+        md = """
+1. Something wrong with me.
+2. Something wrong with me.
+3. Something wrong with me.
+5. What happened to 4?
+"""
+        blocktype = block_to_block_type(md)
+        self.assertEqual(
+            blocktype,
+            blocktype.PARAGRAPH
+        )
+
+    def test_block_to_block_type6(self):
+        md = "Paragraph test"
+        blocktype = block_to_block_type(md)
+        self.assertEqual(
+            blocktype,
+            blocktype.PARAGRAPH
         )
